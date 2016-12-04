c226963ca5aa77faa628c748b1c893143c3734ee
not--p <jason@lakonacomputers.com>
2016-12-03 14:12:28 -1000

    Added tests/FNMTester.java

---
+++ tests/FNMTester.java
@@ -0,0 +1,18 @@
+package spanish.tests;
+
+
+/**
+ * FNMTester
+ * 
+ * Namespace for testing the spanish.FileNameMap class.
+ * 
+ * @author jason
+ */
+
+public class FNMTester 
+{
+    public static void main(String argv[])
+    {
+        System.out.println("Testing FileNameMap...");
+    }
+}
