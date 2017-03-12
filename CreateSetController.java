35806879bb3ad9d01218230cd1fbe4e5bdd492d1
not--p <jason@lakonacomputers.com>
2017-03-11 16:28:13 -1000

    Preliminary Adding Answer and Prompts

--- CreateSetController.java
+++ CreateSetController.java
@@ -7,7 +7,14 @@ package spanish;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 /**
  * FXML Controller class
@@ -16,12 +23,35 @@ import javafx.fxml.Initializable;
  */
 public class CreateSetController implements Initializable {
 
+    public GridPane createSetUI;
+    public Button addItemBtn;
+    public GridPane itemsPane;
+
+    private int items = 0;
+    
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        System.out.println("hi im the cree8 set controller");
-    }    
+        //System.out.println("hi im the cree8 set controller");
+        
+        //createSetUI.add(new Label("First"), 0, 0);
+    }
     
+    
+    public void addItemBtnAction () {
+        items++;
+        System.out.println("There are " + items + " items");
+                
+        TextField prompt = new TextField();
+        TextField answer = new TextField();
+        prompt.setId("prompt-" + items);
+        answer.setId("answer-" + items);
+        
+        itemsPane.addRow(items, prompt, answer);
+    }
+
 }
